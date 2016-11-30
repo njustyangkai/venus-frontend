9e435e6c47bac627f44d286068b4b46a0c1ecf26
杨恺 <njustyangkai@126.com>
2016-11-30 23:27:18 +0800

    cs

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'px-navbar',
@@ -9,8 +10,18 @@ import { Router } from '@angular/router';
 export class NavbarComponent {
   username:string;
 
-  constructor(private router:Router) {
+  @ViewChild('modal1_template') modal1_template:any;
+  modal1:any;
+
+  constructor(private router:Router, private modalService:NgbModal) {
     this.username = window.localStorage.getItem('username');
+  }
+
+  open1() {
+    this.modal1 = this.modalService.open(this.modal1_template, {
+      backdrop: true,
+      keyboard: true
+    });
   }
 
   logout() {
