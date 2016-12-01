5bce6d432ff00892d5b946e3440e4e717193d6d1
杨恺 <njustyangkai@126.com>
2016-12-01 09:47:15 +0800

    css

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -13,12 +13,22 @@ export class NavbarComponent {
   @ViewChild('modal1_template') modal1_template:any;
   modal1:any;
 
+  @ViewChild('modal2_template') modal2_template:any;
+  modal2:any;
+
   constructor(private router:Router, private modalService:NgbModal) {
     this.username = window.localStorage.getItem('username');
   }
 
   open1() {
     this.modal1 = this.modalService.open(this.modal1_template, {
+      backdrop: true,
+      keyboard: true
+    });
+  }
+
+  open2() {
+    this.modal2 = this.modalService.open(this.modal2_template, {
       backdrop: true,
       keyboard: true
     });
