1ec9b1e0d102baa932e683810ff4fe84160c35f3
杨恺 <njustyangkai@126.com>
2016-12-03 11:25:44 +0800

    xs

--- src/app/student/student-add.component.ts
+++ src/app/student/student-add.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './student-add.component.html'
 })
 
 export class StudentAddComponent {
+  constructor(private router:Router) {
 
+  }
+
+  cancel() {
+    this.router.navigate(['/main/student']);
+  }
 }
