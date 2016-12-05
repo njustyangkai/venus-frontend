90989a0e8dd7bc50e067d4333d0d9b3d035b130c
杨恺 <njustyangkai@126.com>
2016-12-05 15:36:57 +0800

    xs

--- src/app/student/student-detail.component.ts
+++ src/app/student/student-detail.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { StudentService } from './student.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './student-detail.component.html'
 })
 
 export class StudentDetailComponent {
+  currentData:any;
 
+  constructor(private studentService:StudentService,
+              private router:Router) {
+    this.currentData = this.studentService.currentData;
+  }
+
+  gotoEdit() {
+    this.router.navigate(['/main/editStudent']);
+  }
 }
