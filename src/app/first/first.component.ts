345e87fe37b69d0e27bf1113ca7d708b8bb37242
杨恺 <njustyangkai@126.com>
2016-12-05 19:43:22 +0800

    xs

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import * as moment from 'moment';
+import { HttpService } from '../util/http.service';
+import { ApiConfig } from '../util/ApiConfig';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './first.component.html'
 })
 
 export class FirstComponent {
+  username:string;
+  isSexChecked:boolean = false;
 
+  isShowSuccess:boolean = false;
+  successMsg:string;
+
+  @ViewChild('editForm') editForm:any;
+
+  constructor(private http:HttpService, private router:Router) {
+    this.username = window.localStorage.getItem('username');
+  }
+
+  sexCheck(e:any) {
+    this.isSexChecked = true;
+  }
+
+  edit() {
+    let formValue = this.editForm.form.value;
+    for (let k in formValue) {
+      if (k === 'birthday') {
+        let tmp = formValue[k].year + '-' + formValue[k].month + '-' + formValue[k].day;
+        let date = new Date(tmp);
+        formValue[k] = moment(date).format('YYYY-MM-DD');
+      }
+    }
+    this.http.post(ApiConfig.STUDENT + '/' + window.localStorage.getItem('userId'), formValue).subscribe(
+        (res:any)=> {
+          if (res.success) {
+            this.successMsg = '提交成功。2s后登陆控制台。';
+            this.isShowSuccess = true;
+            setTimeout(()=> {
+              this.isShowSuccess = false;
+              this.router.navigate(['/main']);
+            }, 2000);
+          }
+        },
+        (error:any)=> {
+          console.log(error);
+        }
+    );
+  }
 }
